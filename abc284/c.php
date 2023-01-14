602dd911a30aa4942c961b7684da9f2133204bd0
Yusuke Egami <egpersent@gmail.com>
2023-01-15 02:02:55 +0900

    abc284(php)をUnionFindで実装

--- abc284/c.php
+++ abc284/c.php
@@ -1,48 +1,63 @@
 <?php
 
-$cc_list = [];
 list($n, $m) = explode(' ', trim(fgets(STDIN)));
-
+$uf = new UnionFind($n);
 for ($i = 0; $i < $m; $i++) {
     list($u, $v) = explode(' ', trim(fgets(STDIN)));
-    $u_in_cc = null;
-    $v_in_cc = null;
-    foreach ($cc_list as $key => $cc) {
-        if (in_array($u, $cc)) {
-            $cc_list[$key][] = $v;
-            $cc_list[$key] = array_unique($cc_list[$key], SORT_NUMERIC);
-            $u_in_cc = $key;
-        }
-        if (in_array($v, $cc)) {
-            $cc_list[$key][] = $u;
-            $cc_list[$key] = array_unique($cc_list[$key], SORT_NUMERIC);
-            $v_in_cc = $key;
+    $uf->union($u - 1, $v - 1);
+}
+$ans = 0;
+for ($i = 0; $i < $n; $i++) {
+    if ($uf->root($i) === $i) {
+        $ans++;
+    }
+}
+echo $ans;
+
+
+class UnionFind
+{
+    private $pr = [];
+    private $sz = [];
+
+    public function __construct($n)
+    {
+        for ($i = 0; $i < $n; $i++) {
+            $this->pr[$i] = $i;
+            $this->sz[$i] = 1;
         }
     }
 
-    // 新しい連結成分
-    if ($u_in_cc === null && $v_in_cc === null) {
-        $cc_list[] = [$u, $v];
-    } else if ($u_in_cc !== null && $v_in_cc !== null) {
-        if ($u_in_cc !== $v_in_cc) {
-            // 二つの連結成分を結合して一つの連結成分を生成する
-            $cc_list[$u_in_cc] = array_unique(array_merge($cc_list[$u_in_cc], $cc_list[$v_in_cc]));
-            unset($cc_list[$v_in_cc]);
+    public function root($a)
+    {
+        // 自身がrootならroot発見
+        if ($this->pr[$a] === $a) {
+            return $a;
         }
+        // 再帰的に親のrootを探索する
+        return $this->root($this->pr[$a]);
+    }
+
+    public function union($a, $b)
+    {
+        $a = $this->root($a);
+        $b = $this->root($b);
+
+        // rootが同じなら何もしない
+        if ($a === $b) {
+            return;
+        }
+
+        if ($this->sz[$a] < $this->sz[$b]) {
+            list($a, $b) = [$b, $a];
+        }
+        // sizeが小さい方のrootに大きい方のrootをセットする
+        $this->pr[$b] = $a;
+        $this->sz[$a] += $this->sz[$b];
+    }
+
+    public function isSame($a, $b)
+    {
+        return $this->root($a) === $this->root($b);
     }
 }
-// 一度も入力がなかった頂点の数
-$add_count = 0;
-foreach (range(1, $n) as $node) {
-    $hit = false;
-    foreach ($cc_list as $cc) {
-        if (in_array($node, $cc)) {
-            $hit = true;
-            break;
-        }
-    }
-    if ($hit === false) {
-        $add_count++;
-    }
-}
-echo (count($cc_list) + $add_count) . PHP_EOL;
